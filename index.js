d91fd0e17e19ff6639cfa1fd980addbd08b6e83f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-01-16 17:41:30 +0530

    update project

---
+++ index.js
@@ -0,0 +1,16 @@
+const buttonMark = document.querySelector(".read-mark");
+const firstList = document.querySelector(".first");
+const secondList = document.querySelector(".second");
+const thirdList = document.querySelector(".third");
+const redDot = document.getElementsByClassName("red-dot");
+const count = document.querySelector(".count");
+
+buttonMark.addEventListener('click', changes) 
+
+function changes() {
+    firstList.style.backgroundColor = "white";
+    secondList.style.backgroundColor = "white";
+    thirdList.style.backgroundColor = "white";
+    count.style.visibility = "hidden";
+    redDot.style.content = "";
+}
